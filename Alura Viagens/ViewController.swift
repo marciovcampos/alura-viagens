7ef2e3ceaff78bc366592546fbe1b48f45ec5b85
Márcio Vinícius <marcio.campos@outlook.com>
2020-06-26 10:45:40 -0300

    first commit

---
+++ Alura Viagens/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alura Viagens
+//
+//  Created by Marcio Vinicius Campos da Silveira on 26/06/20.
+//  Copyright © 2020 MarcioVCampos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
