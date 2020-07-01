1a38ef6dff326c8e0f4ab72e9d69ce764595af19
Márcio Vinícius <marcio.campos@outlook.com>
2020-07-01 12:40:13 -0300

    criando tela detalhes viagem

---
+++ Alura Viagens/DetalhesViagem/Controller/DetalhesViagensViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetalhesViagensViewController.swift
+//  Alura Viagens
+//
+//  Created by Marcio Vinicius Campos da Silveira on 01/07/20.
+//  Copyright © 2020 MarcioVCampos. All rights reserved.
+//
+
+import UIKit
+
+class DetalhesViagensViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
