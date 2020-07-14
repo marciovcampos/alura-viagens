fe67909fc292b63dd25d92fdf0c4a0566d31c8b4
Márcio Vinícius <marcio.campos@outlook.com>
2020-07-14 18:01:39 -0300

    refatorando action botaoVoltar detalhesViagem

--- Alura Viagens/DetalhesViagem/Controller/DetalhesViagensViewController.swift
+++ Alura Viagens/DetalhesViagem/Controller/DetalhesViagensViewController.swift
@@ -39,7 +39,7 @@ class DetalhesViagensViewController: UIViewController {
     }
     
     @IBAction func botaoVoltar(_ sender: UIButton) {
-        self.dismiss(animated: true, completion: nil)
+        self.navigationController?.popViewController(animated: true)
        }
     
     @IBAction func textFieldEntrouFoco(_ sender: UITextView) {
